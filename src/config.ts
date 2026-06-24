06d4b8222b1a27fa33cb522698aca428f4716ebc
Bury <18151161@qq.com>
2026-06-25 01:48:39 +0800

    更新配置

--- src/config.ts
+++ src/config.ts
@@ -294,12 +294,6 @@ export const navBarConfig: NavBarConfig = {
 					external: true,
 					icon: "fa7-brands:bilibili",
 				},
-				{
-					name: "Gitee",
-					url: "https://gitee.com/matsuzakayuki/Mizuki",
-					external: true,
-					icon: "mdi:git",
-				},
 			],
 		},
 		{
